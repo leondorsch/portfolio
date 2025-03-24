53453dc5de7307be853f039d149ad5a8111e6378
Leon Dorsch <leon.dorsch@gmx.de>
2025-03-24 22:37:12 +0100

    first construct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { ReferencesComponent } from './references/references.component';
     AboutMeComponent,
     SkillsComponent,
     PortfolioComponent,
-    ReferencesComponent
+    ReferencesComponent,
+    ContactComponent,
+    FooterComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
